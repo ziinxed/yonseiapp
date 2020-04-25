2aad6b4c39b646cafad1cf603c7b673057643ada
DEOK <ziinxed@gmail.com>
2020-04-25 20:15:31 +0900

    listFileTXT

--- haksul.py
+++ haksul.py
@@ -8,15 +8,24 @@ print(type(soup))
 noticeList = soup.select('td a')
 
 noticeLinkList = list()
+f = open("listFile.txt", 'w')
+
 
 for link in noticeList:
     if 'href' in link.attrs:
-        print(link.attrs['href'], type(link.attrs['href']))
-        noticeLinkList.append('library.yonsei.ac.kr' + link.attrs['href'])
+        #print(link.attrs['href'], type(link.attrs['href']))
+        listLink = 'library.yonsei.ac.kr' + link.attrs['href']
+        noticeLinkList.append(listLink)
+        f.write(listLink + "\n")
+
+f.close()
+
+for link in noticeLinkList:
+    print(link)
 
 
 
-print(noticeLinkList)
-print (type(noticeList), len(noticeList))
-print (noticeList)
+#print(noticeLinkList)
+#print (type(noticeList), len(noticeList))
+#print (noticeList)
 
